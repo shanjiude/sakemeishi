3a4659b19edb6687bb84f0552988b139a0f786b1
shanjiude <shanjiude@gmail.com>
2025-02-21 13:06:26 +0900

    ロゴとタイトルを修正

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>お酒の名刺</title>
+        <link rel="icon" type="image/png" href="{{ asset('cheers.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
