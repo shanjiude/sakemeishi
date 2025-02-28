55973053f298f84584f0f206c02024c24f0e5abd
AOaOA07 <def1del15pesperf1c10@gmail.com>
2025-02-28 14:56:57 +0900

    ハンバーガーメニューCSS一旦プッシュ

--- app/resources/views/components/header.blade.php
+++ app/resources/views/components/header.blade.php
@@ -10,23 +10,27 @@
         <div class="hamburger-menu" style="position: absolute; right: 20px; top: 20px;"> <!-- 右上に配置 -->
                 <!-- HTML -->
                 <button class="hamburger-btn">
-                    &#9776; メニュー
+                    &#9776;
                 </button>
 
                 <div id="profileMenu" class="profile-menu hidden">
-                    <div class="profile-edit">
+                    <div class="hb-profile-edit">
                         <a href="{{ route('account.edit') }}">プロフィール編集</a>
                     </div>
-                    <div class="ID-search">
+                    <div class="hb-ID-search">
                         <a href="{{ route('account.search') }}">ID検索</a>
                     </div>
-                    <div class="ID-search">
+                    <div class="hb-ID-search">
                         <a href="{{ route('rakuten.search') }}">最近飲んだお酒の登録</a>
                     </div>
-                    <div class="go-followlist">
+                    <div class="hb-go-followlist">
                         <a href="{{ route('followlist.index') }}">フォロー一覧</a>
                     </div>
                 </div>
         </div>
     </div>
 </header>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
