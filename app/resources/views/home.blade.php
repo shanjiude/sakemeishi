c71e74dad888456aafe0d327535ef4961efbb9e7
shanjiude <shanjiude@gmail.com>
2025-02-28 15:16:56 +0900

    未ログイン時の説明画像追加

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 <article>
     <div class="container">
-        <p>お酒の好みを共有しよう！</p>
+        <p>お酒の好みを共有しよう</p>
         <a href="{{ route('login') }}" class="btn">ログイン</a>
         <a href="{{ route('register') }}" class="btn">新規登録</a>
+    </div>
+    <div class="canva-embed">
+        <iframe loading="lazy"
+            src="https://www.canva.com/design/DAGgXRP_dJc/R-5l7mG35DvoAL1NC9MiUQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+        </iframe>
     </div>
 </article>
 @endsection
