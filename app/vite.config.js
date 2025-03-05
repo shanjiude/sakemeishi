1200a085cf4aecdbe23ece0ba12390b9f31c432d
shanjiude <shanjiude@gmail.com>
2025-03-05 11:44:08 +0900

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5174,
+        strictPort: true,
+        hmr: {
+        host: '192.168.0.33'
+        },
+        watch: {
+        usePolling: true
+        }
+    }
 });
