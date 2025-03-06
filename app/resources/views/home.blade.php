12d39532909e59c0b1bed5d9d438a6947b9dacb5
AOaOA07 <def1del15pesperf1c10@gmail.com>
2025-03-06 12:29:44 +0900

    !マーク追加

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <article>
     <div class="container">
-        <p>お酒の好みを共有しよう</p>
+        <p>お酒の好みを共有しよう！</p>
         <a href="{{ route('login') }}" class="btn">ログイン</a>
         <a href="{{ route('register') }}" class="btn">新規登録</a>
     </div>
